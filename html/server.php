f1423460eb1c2577f887017fff873afa570e7b23
aunepvp <leo.mhs@icloud.com>
2022-05-10 18:54:53 -0700

    Doesn't print Query output. Only stores it in Variable.

--- html/server.php
+++ html/server.php
@@ -4,11 +4,7 @@ $var2 = ('" --host "');
 $var3 = ('" --port "');
 $var4 = ('"');
 $var5 = "--requestRules: true";
-$type = $_GET['type'];
-$ip = $_GET['ip'];
-$qport = $_GET['qport'];
 $command = $var1 . $type . $var2 . $ip . $var3 . $qport .  $var4;
-$output = shell_exec($command);
-echo $output;
+$serverstatus = shell_exec($command);
 ?>
 
