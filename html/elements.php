08fbff8543d112964934d232bfbfd6e45a751093
aunepvp <leo.mhs@icloud.com>
2022-05-10 20:29:46 -0700

    Deleted Unused File.

--- html/elements.php
+++
@@ -1,9 +0,0 @@
-<table id="server_list_table"><tbody>
-                <tr id="server_list_table_top">
-                    <th class="status_cell">Status</th>
-                    <th class="connectlink_cell">Adresse</th>
-                    <th class="servername_cell">Name</th>
-                    <th class="players_cell">Spieler</th>
-                    <th class="img-cell"><div></div></th>
-                </tr></tbody>
-              </table>
