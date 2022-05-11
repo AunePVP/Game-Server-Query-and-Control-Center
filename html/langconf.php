e21f1ae1c5138d832bded0326d9d317e9ee415b3
aunepvp <leo.mhs@icloud.com>
2022-05-10 20:01:49 -0700

    Language Configuration File.

---
+++ html/langconf.php
@@ -0,0 +1,29 @@
+<?php
+if ($lang == "en") {
+    $Überblick = "General";
+    $Control = "Control";
+    $Config = "Config";
+    $Spieler = "Players";
+    $seit = "since";
+    $Status = "Status";
+    $Adresse = "IP";
+    $Name = "Title";
+} elseif ($lang == "de") {
+    $Überblick = "Überblick";
+    $Control = "Control";
+    $Config = "Config";
+    $Spieler = "Spieler";
+    $seit = "seit";
+    $Status = "Status";
+    $Adresse = "Adresse";
+    $Name = "Name";
+} else {
+    $Überblick = "General";
+    $Control = "Control";
+    $Config = "Config";
+    $Spieler = "Players";
+    $seit = "since";
+    $Status = "Status";
+    $Adresse = "IP";
+    $Name = "Title";
+}
