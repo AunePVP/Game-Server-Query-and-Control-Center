18d5ebe57eafcd7b29946e5b54c6e7a17ec2d55b
aunepvp <leo.mhs@icloud.com>
2022-05-12 19:34:33 -0700

    Redesigned CSS

--- users/login.php
+++ users/login.php
@@ -2,50 +2,45 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>
-        Login and Registration
-        System - LAMP Stack
-    </title>
-
-    <link rel="stylesheet" type="text/css"
-          href="style.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <style>
+        body{ font: 14px sans-serif; height: 100vh; display: flex;}
+        .wrapper{ width: 360px; padding: 20px; margin: auto; }
+    </style>
 </head>
 <body>
-<div class="header">
-    <h2>Login Here!</h2>
+<div class="wrapper">
+    <h2>Login</h2>
+
+    <form method="post" action="login.php">
+
+        <?php include('error.php'); ?>
+
+        <div class="form-group">
+            <label>Username</label>
+            <input type="text" name="username" class="form-control">
+        </div>
+        <div class="form-group">
+            <label>Password</label>
+            <input type="password" name="password" class="form-control">
+        </div>
+        <div class="form-group">
+            <button type="submit" class="btn btn-primary"
+                    name="login_user">
+                Login
+            </button>
+        </div>
+        <p>
+            New Here?
+            <a href="register.php">
+                Click here to register!
+            </a>
+        </p>
+    </form>
 </div>
-
-<form method="post" action="login.php">
-
-    <?php include('error.php'); ?>
-
-    <div class="input-group">
-        <label>Enter Username</label>
-        <input type="text" name="username" >
-    </div>
-    <div class="input-group">
-        <label>Enter Password</label>
-        <input type="password" name="password">
-    </div>
-    <div class="input-group">
-        <button type="submit" class="btn"
-                name="login_user">
-            Login
-        </button>
-    </div>
-
-
-
-    <p>
-        New Here?
-        <a href="register.php">
-            Click here to register!
-        </a>
-    </p>
-
-
-
-</form>
 </body>
 
 </html>
