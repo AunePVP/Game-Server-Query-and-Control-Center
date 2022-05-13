45d1194ca6ff9c47f0fb0c3a28ffb9c8968a33ee
aunepvp <leo.mhs@icloud.com>
2022-05-12 19:53:47 -0700

    Added user image

--- html/nav.php
+++ html/nav.php
@@ -1,13 +1,14 @@
 <div id="nav">
       <ul>
-       <li><a href="">Gameserver</a></li>
+          <li><a href="">Gameserver</a></li>
+          <div style="width: 100%;"></div>
           <?php
           session_start();
           if (isset($_SESSION['username'])) {
               $username = $_SESSION['username'];
-              echo '<div class="dropdown"><button class="dropbtn">'.$username.'</button><div class="dropdown-content"><a href="users/index.php">HI</a></div></div>';
+              echo '<div style="height: 58px;"><a href="users/index.php"><img class="user-avater" src="https://avatars.akamai.steamstatic.com/ba20937c5bba10858664e3f32f7d2cc9e96a5275_full.jpg"></a></div>';
           }else {
-              echo '<div class="dropdown"><button class="dropbtn" href="users/index.php">Login</button><div class="dropdown-content"><a href="users/index.php">HI</a></div></div>';
+              echo '<div class="login"><a class="button white-hover" href='."'users/index.php'".'">Login</a><div class="dropdown-content"></div></div>';
           }
           ?>
       </div>
