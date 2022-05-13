405681aa38315e91b6a9b5c94f4747c7086b5d65
aunepvp <leo.mhs@icloud.com>
2022-05-12 19:33:27 -0700

    Added color scheme and cleaned up the code.

--- html/head.php
+++ html/head.php
@@ -1,35 +1,35 @@
 <head>
-<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-<!-- this is the part responsible for hidding the bottom bar -->
-<meta name="apple-mobile-web-app-capable" content="yes" />
-<meta names="apple-mobile-web-app-status-bar-style" content="black-translucent" />
-<meta charset="utf-8">
-<title>Gamserver Query</title>
-<link rel="stylesheet" href="css/style.css">
-<link rel="stylesheet" href="
-<?php 
-if ($css_template == "light") {
-    echo "css/light.css";
-} elseif ($css_template == "dark") {
-	echo "css/dark.css";
-}
-?>">
-<link rel="apple-touch-icon" sizes="57x57" href="ico/apple-icon-57x57.png">
-<link rel="apple-touch-icon" sizes="60x60" href="ico/apple-icon-60x60.png">
-<link rel="apple-touch-icon" sizes="72x72" href="ico/apple-icon-72x72.png">
-<link rel="apple-touch-icon" sizes="76x76" href="ico/apple-icon-76x76.png">
-<link rel="apple-touch-icon" sizes="114x114" href="ico/apple-icon-114x114.png">
-<link rel="apple-touch-icon" sizes="120x120" href="ico/apple-icon-120x120.png">
-<link rel="apple-touch-icon" sizes="144x144" href="ico/apple-icon-144x144.png">
-<link rel="apple-touch-icon" sizes="152x152" href="ico/apple-icon-152x152.png">
-<link rel="apple-touch-icon" sizes="180x180" href="ico/apple-icon-180x180.png">
-<link rel="icon" type="image/png" sizes="192x192"  href="ico/android-icon-192x192.png">
-<link rel="icon" type="image/png" sizes="32x32" href="ico/favicon-32x32.png">
-<link rel="icon" type="image/png" sizes="96x96" href="ico/favicon-96x96.png">
-<link rel="icon" type="image/png" sizes="16x16" href="ico/favicon-16x16.png">
-<link rel="manifest" href="ico/manifest.json">
-<meta name="msapplication-TileColor" content="#ffffff">
-<meta name="msapplication-TileImage" content="ico/ms-icon-144x144.png">
-<meta name="theme-color" content="#ffffff">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"/>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <!-- this is the part responsible for hidding the bottom bar -->
+    <meta name="apple-mobile-web-app-capable" content="yes"/>
+    <meta names="apple-mobile-web-app-status-bar-style" content="black-translucent"/>
+    <meta name="theme-color" content="#1D1D1F" media="(prefers-color-scheme: light)">
+    <meta name="theme-color" content="#1D1D1F" media="(prefers-color-scheme: dark)">
+    <meta charset="utf-8">
+    <title>Gamserver Query</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="<?php
+    if ($css_template == "light") {
+        echo "css/light.css";
+    } elseif ($css_template == "dark") {
+        echo "css/dark.css";
+    } ?>">
+    <link rel="apple-touch-icon" sizes="57x57" href="ico/apple-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="ico/apple-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="ico/apple-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="ico/apple-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="ico/apple-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="ico/apple-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="ico/apple-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="ico/apple-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="ico/apple-icon-180x180.png">
+    <link rel="icon" type="image/png" sizes="192x192" href="ico/android-icon-192x192.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="ico/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="ico/favicon-96x96.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="ico/favicon-16x16.png">
+    <link rel="manifest" href="ico/manifest.json">
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="msapplication-TileImage" content="ico/ms-icon-144x144.png">
+    <meta name="theme-color" content="#ffffff">
 </head>
