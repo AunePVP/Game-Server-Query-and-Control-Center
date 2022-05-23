eee6d56f27704513ce7a5cee33ea2d2b5f2e78b0
AunePVP <67545895+AunePVP@users.noreply.github.com>
2022-05-22 22:45:52 -0700

    Update index.php

--- html/type/minecraft/index.php
+++ html/type/minecraft/index.php
@@ -1,15 +1,14 @@
 <?php
 // This is where I'm querying all the data I need and storing it in variables.
-$countplayers = $serverstatus->players->online;
-$maxplayers = $serverstatus->players->max;
-$titlename = $serverstatus->motd->html[0];
-$title = $serverstatus->motd->clean[0];
-$img = "https://api.mcsrvstat.us/icon/$ip";
-$connectlink = $ip . ":" . $port;
-$version = $serverstatus->version;
+$countplayers = $battlemetrics_serverstatus->data->attributes->players;
+$maxplayers = $battlemetrics_serverstatus->data->attributes->maxPlayers;
+$title = $battlemetrics_serverstatus->data->attributes->name;
+$titlename = $battlemetrics_serverstatus->data->attributes->name;
+$img = "https://cdn.muehlhaeusler.online/img/tracker/game-logos/valheim.png";
+$connectlink = "steam://connect/$ip:$port";
 
 // If the server is offline, the variable will be empty. That' how I check if the server is online.
-$status = $serverstatus->online;
+$status = $battlemetrics_serverstatus->data->attributes->status;
 if (isset($status)) {
     $status = 1;
 } else {
