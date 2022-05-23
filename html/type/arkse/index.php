d969b4345071f299dd9236d5ea0a4d66815376bb
AunePVP <67545895+AunePVP@users.noreply.github.com>
2022-05-22 22:45:18 -0700

    Update index.php

--- html/type/arkse/index.php
+++ html/type/arkse/index.php
@@ -1,14 +1,20 @@
 <?php
 // This is where I'm querying all the data I need and storing it in variables.
-$countplayers = $battlemetrics_serverstatus->data->attributes->players;
-$maxplayers = $serverstatus->maxplayers;
-$title = $serverstatus->name;
+$countplayers = 0;
+foreach ($serverstatus->players as $player) {
+    if(!strlen($player->Name))
+        continue;
+    $countplayers = $countplayers + 1;
+}
+//$countplayers = $battlemetrics_serverstatus->data->attributes->players;
+$maxplayers = $serverstatus->info->MaxPlayers;
+$title = $serverstatus->info->HostName;
+$titlename = $serverstatus->info->HostName;
 $img = "https://cdn.muehlhaeusler.online/img/tracker/game-logos/ark.png";
 $connectlink = "steam://connect/$ip:$gport";
-$map = $serverstatus->map;
-$ping = $serverstatus->ping;
-// If the server is offline, the variable will be empty. That's how I check if the server is online.
-$status = $serverstatus->raw->numplayers;
+$map = $serverstatus->info->Map;
+// If the server is offline, the variable will be empty. That' how I check if the server is online.
+$status = $serverstatus->info->Players;
 if (isset($status)) {
     $status = 1;
 } else {
