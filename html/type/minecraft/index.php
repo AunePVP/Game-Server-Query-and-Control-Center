7a8f5040a72d898ae5eab89ba607c7b084959cdc
aunepvp <leo.mhs@icloud.com>
2022-05-22 23:07:42 -0700

    Repair

--- html/type/minecraft/index.php
+++ html/type/minecraft/index.php
@@ -1,14 +1,15 @@
 <?php
 // This is where I'm querying all the data I need and storing it in variables.
-$countplayers = $battlemetrics_serverstatus->data->attributes->players;
-$maxplayers = $battlemetrics_serverstatus->data->attributes->maxPlayers;
-$title = $battlemetrics_serverstatus->data->attributes->name;
-$titlename = $battlemetrics_serverstatus->data->attributes->name;
-$img = "https://cdn.muehlhaeusler.online/img/tracker/game-logos/valheim.png";
-$connectlink = "steam://connect/$ip:$port";
+$countplayers = $serverstatus->players->online;
+$maxplayers = $serverstatus->players->max;
+$titlename = $serverstatus->motd->html[0];
+$title = $serverstatus->motd->clean[0];
+$img = "https://api.mcsrvstat.us/icon/$ip";
+$connectlink = $ip . ":" . $port;
+$version = $serverstatus->version;
 
 // If the server is offline, the variable will be empty. That' how I check if the server is online.
-$status = $battlemetrics_serverstatus->data->attributes->status;
+$status = $serverstatus->online;
 if (isset($status)) {
     $status = 1;
 } else {
--- html/type/valheim/index.php
+++ html/type/valheim/index.php
@@ -1,13 +1,14 @@
 <?php
 // This is where I'm querying all the data I need and storing it in variables.
-$countplayers = $serverstatus->raw->numplayers;
-$maxplayers = $serverstatus->maxplayers;
-$title = $serverstatus->name;
-$img = "html/img/valheim.png";
+$countplayers = $battlemetrics_serverstatus->data->attributes->players;
+$maxplayers = $battlemetrics_serverstatus->data->attributes->maxPlayers;
+$title = $battlemetrics_serverstatus->data->attributes->name;
+$titlename = $battlemetrics_serverstatus->data->attributes->name;
+$img = "https://cdn.muehlhaeusler.online/img/tracker/game-logos/valheim.png";
 $connectlink = "steam://connect/$ip:$port";
 
 // If the server is offline, the variable will be empty. That' how I check if the server is online.
-$status = $serverstatus->raw->numplayers;
+$status = $battlemetrics_serverstatus->data->attributes->status;
 if (isset($status)) {
     $status = 1;
 } else {
