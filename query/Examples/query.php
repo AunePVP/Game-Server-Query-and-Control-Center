61fa61616e997a9d03e231f2dad558954f6cca78
aunepvp <leo.mhs@icloud.com>
2022-05-23 08:01:54 -0700

    Added Query.php

---
+++ query/Examples/query.php
@@ -0,0 +1,40 @@
+<?php
+require __DIR__ . '/../SourceQuery/bootstrap.php';
+
+use xPaw\SourceQuery\SourceQuery;
+
+// For the sake of this example
+header( 'Content-Type: text/plain' );
+header( 'X-Content-Type-Options: nosniff' );
+
+// Edit this ->
+
+$ip = $_GET['ip'];
+$port = $_GET['port'];
+define( 'SQ_TIMEOUT',     1 );
+define( 'SQ_ENGINE',      SourceQuery::SOURCE );
+// Edit this <-
+$Query = new SourceQuery( );
+
+try
+{
+    $Query->Connect( $ip, $port, SQ_TIMEOUT, SQ_ENGINE );
+
+    $data["info"] = $Query->GetInfo();
+    $data["info"]["Game"] = $game;
+    $data["info"]["IP"] = $ip;
+    $data["info"]["Port"] = $port;
+    $data["players"] = $Query->GetPlayers();
+    $data["rules"] = $Query->GetRules();
+
+    $data = json_encode($data);
+    echo $data;
+}
+catch( Exception $e )
+{
+    echo $e->getMessage( );
+}
+finally
+{
+    $Query->Disconnect( );
+}
