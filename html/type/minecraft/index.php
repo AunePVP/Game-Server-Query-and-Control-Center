dcebbedf39f8e9a8db7426053e4ad4f69370a12d
AunePVP <67545895+AunePVP@users.noreply.github.com>
2022-05-22 22:45:39 -0700

    Update index.php

--- html/type/minecraft/index.php
+++ html/type/minecraft/index.php
@@ -1,14 +1,15 @@
 <?php
 // This is where I'm querying all the data I need and storing it in variables.
-$countplayers = $serverstatus->raw->vanilla->raw->players->online;
-$maxplayers = $serverstatus->raw->vanilla->raw->players->max;
-$title = $serverstatus->name;
+$countplayers = $serverstatus->players->online;
+$maxplayers = $serverstatus->players->max;
+$titlename = $serverstatus->motd->html[0];
+$title = $serverstatus->motd->clean[0];
 $img = "https://api.mcsrvstat.us/icon/$ip";
 $connectlink = $ip . ":" . $port;
-$version = $serverstatus->raw->vanilla->raw->version->name;
+$version = $serverstatus->version;
 
 // If the server is offline, the variable will be empty. That' how I check if the server is online.
-$status = $serverstatus->raw->vanilla->raw->players->online;
+$status = $serverstatus->online;
 if (isset($status)) {
     $status = 1;
 } else {
