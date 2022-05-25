b11a0c7ab6d06fbb03a35321170309dcbfcaf330
aunepvp <leo.mhs@icloud.com>
2022-05-24 18:15:34 -0700

    Get Mods from Ark Query

--- html/type/arkse/index.php
+++ html/type/arkse/index.php
@@ -13,6 +13,18 @@ $titlename = $serverstatus->info->HostName;
 $img = "https://cdn.muehlhaeusler.online/img/tracker/game-logos/ark.png";
 $connectlink = "steam://connect/$ip:$gport";
 $map = $serverstatus->info->Map;
+$keys = (array) $serverstatus->rules;
+$matched = preg_grep('/MOD\d+_s/', array_keys($keys));
+foreach ($matched as $modkey) {
+    $modcontent = $serverstatus->rules->{$modkey};
+    if (!isset($value)) {
+        $value = 0;
+    } else {
+        $value = $value + 1;
+    }
+    $modcontent = substr($modcontent, 0, 10);
+    $mods[$value] = $modcontent;
+}
 // If the server is offline, the variable will be empty. That' how I check if the server is online.
 $status = $serverstatus->info->Players;
 if (isset($status)) {
