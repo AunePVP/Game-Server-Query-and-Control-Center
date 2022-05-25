738b578771f9c483b5bcfe79b239a8b57b133a28
aunepvp <leo.mhs@icloud.com>
2022-05-24 18:16:12 -0700

    Convert old minecraft query methode to new

--- html/type/minecraft/index.php
+++ html/type/minecraft/index.php
@@ -2,14 +2,20 @@
 // This is where I'm querying all the data I need and storing it in variables.
 $countplayers = $serverstatus->players->online;
 $maxplayers = $serverstatus->players->max;
-$titlename = $serverstatus->motd->html[0];
-$title = $serverstatus->motd->clean[0];
-$img = "https://api.mcsrvstat.us/icon/$ip";
-$connectlink = $ip . ":" . $port;
+$titlename = "";
+foreach ($serverstatus->description->extra as $extra) {
+    $titlename.= $extra->text;
+}
+if (empty($titlename)) {
+    $titlename = $serverstatus->description;
+}
+$title = $titlename;
+$img = $serverstatus->favicon;
+$connectlink = $ip . ":" . $gport;
 $version = $serverstatus->version;
 
 // If the server is offline, the variable will be empty. That' how I check if the server is online.
-$status = $serverstatus->online;
+$status = $serverstatus->players->max;
 if (isset($status)) {
     $status = 1;
 } else {
