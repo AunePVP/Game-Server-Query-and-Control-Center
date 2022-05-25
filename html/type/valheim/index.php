d8bea006434242e34401979dce433e6e934f15bf
aunepvp <leo.mhs@icloud.com>
2022-05-24 18:16:34 -0700

    Update old valheim query methode to new

--- html/type/valheim/index.php
+++ html/type/valheim/index.php
@@ -1,14 +1,14 @@
 <?php
 // This is where I'm querying all the data I need and storing it in variables.
-$countplayers = $battlemetrics_serverstatus->data->attributes->players;
-$maxplayers = $battlemetrics_serverstatus->data->attributes->maxPlayers;
-$title = $battlemetrics_serverstatus->data->attributes->name;
-$titlename = $battlemetrics_serverstatus->data->attributes->name;
+$countplayers = $serverstatus->info->Players;
+$maxplayers = $serverstatus->info->MaxPlayers;
+$title = $serverstatus->info->HostName;
+$titlename = $serverstatus->info->HostName;
 $img = "https://cdn.muehlhaeusler.online/img/tracker/game-logos/valheim.png";
-$connectlink = "steam://connect/$ip:$port";
+$connectlink = "steam://connect/$ip:$gport";
 
 // If the server is offline, the variable will be empty. That' how I check if the server is online.
-$status = $battlemetrics_serverstatus->data->attributes->status;
+$status = $serverstatus->info->Protocol;
 if (isset($status)) {
     $status = 1;
 } else {
