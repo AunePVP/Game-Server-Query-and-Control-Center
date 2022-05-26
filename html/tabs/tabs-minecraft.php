2e0f69a3260f82020d2192a5d65e91423eef79f8
aunepvp <leo.mhs@icloud.com>
2022-05-26 15:59:28 -0700

    Add minecraft tabs.

---
+++ html/tabs/tabs-minecraft.php
@@ -0,0 +1,20 @@
+<?php
+include './html/langconf.php';
+?>
+<!DOCTYPE html>
+<table>
+    <tr>
+        <td>Maria Anders</td>
+        <td class="width35 mcquery">
+            <?php
+            if ($qport == 0):
+            echo "Query is disabled on this server.";
+            else:
+            ?>
+            <div>Query is enabled</div>
+            <?php
+            endif;
+            ?>
+        </td>
+    </tr>
+</table>
