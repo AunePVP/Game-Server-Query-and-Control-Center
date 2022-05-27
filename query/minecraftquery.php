747bf9dbe0bbbc0bc3c828fd2ceca0869d0fdcd3
aunepvp <leo.mhs@icloud.com>
2022-05-26 18:11:39 -0700

    Added Query Protocol to file and check which protocl to use

--- query/minecraftquery.php
+++ query/minecraftquery.php
@@ -1,26 +1,37 @@
 <?php
-require __DIR__ . '/minecraft/src/MinecraftPing.php';
-require __DIR__ . '/minecraft/src/MinecraftPingException.php';
-
 use xPaw\MinecraftPing;
 use xPaw\MinecraftPingException;
+use xPaw\MinecraftQuery;
+use xPaw\MinecraftQueryException;
+if ($qport == 0) {
+    require __DIR__ . '/minecraft/src/MinecraftPing.php';
+    require __DIR__ . '/minecraft/src/MinecraftPingException.php';
+    try {
+        $Query = new MinecraftPing($ip, $gport);
 
-try
-{
-    $Query = new MinecraftPing( $ip, $gport );
+        $queryresult = $Query->Query();
+        $queryresult = json_encode($queryresult);
+    } catch (MinecraftPingException $e) {
+        echo $e->getMessage();
+    } finally {
+        if ($Query) {
+            $Query->Close();
+        }
+    }
+}
 
-    $queryresult = $Query->Query();
-    $queryresult = json_encode($queryresult);
-}
-catch( MinecraftPingException $e )
-{
-    echo $e->getMessage();
-}
-finally
-{
-    if( $Query )
-    {
-        $Query->Close();
+else {
+    require __DIR__ . '/minecraft/src/MinecraftQuery.php';
+    require __DIR__ . '/minecraft/src/MinecraftQueryException.php';
+    $Query = new MinecraftQuery();
+    try {
+        $Query->Connect($ip, $qport);
+
+        $queryresult["info"] = ($Query->GetInfo());
+        $queryresult["players"] = ($Query->GetPlayers());
+        $queryresult = json_encode($queryresult);
+    } catch (MinecraftQueryException $e) {
+        echo $e->getMessage();
     }
 }
 ?>
