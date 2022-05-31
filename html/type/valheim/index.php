79a38dc79dee06ba8e43e269d2a90ff6413f34df
aunepvp <leo.mhs@icloud.com>
2022-05-30 17:41:24 -0700

    Get data for banner

--- html/type/valheim/index.php
+++ html/type/valheim/index.php
@@ -1,4 +1,19 @@
 <?php
+// function to get last lines of file
+require 'html/lloffile.php';
+// Get uptime and round uptime for banner
+$uptime = file_get_contents("query/cron/uptime/$ServerID");
+$uptimebanner = str_replace(".", ",", round($uptime, 1))."%";
+// Get last players for banner
+$lastplayerlines = tailCustom("query/cron/$ServerID.json", 7);
+//$lastplayers = preg_split("/\r\n|\n|\r/", $lastplayerlines);
+$playerlineexit = array();
+$count = 0;
+foreach(preg_split("/((\r?\n)|(\r\n?))/", $lastplayerlines) as $playerline){
+    $playerlinedecode = json_decode($playerline);
+    $lastplayers[$count] = $playerlinedecode->players;
+    $count = $count + 1;
+}
 // This is where I'm querying all the data I need and storing it in variables.
 $uptime = file_get_contents("query/cron/uptime/$ServerID")."%";
 $countplayers = $serverstatus->info->Players;
