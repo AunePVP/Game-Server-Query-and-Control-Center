181107312eae3849d7586e8197c14e5e66e333a3
aunepvp <leo.mhs@icloud.com>
2022-06-01 19:54:26 -0700

    Remove useless line: $queryresult = $queryresult;

--- query/minecraftquery.php
+++ query/minecraftquery.php
@@ -29,7 +29,6 @@ else {
 
         $queryresult["info"] = ($Query->GetInfo());
         $queryresult["players"] = ($Query->GetPlayers());
-        $queryresult = $queryresult;
         $queryresult = json_encode($queryresult);
     } catch (MinecraftQueryException $e) {
         echo $e->getMessage();
