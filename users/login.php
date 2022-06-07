115d28303f690e9180a252f3b773aaa72c217a8b
aunepvp <leo.mhs@icloud.com>
2022-06-06 17:15:32 -0700

    Display Navbar in login.php

--- users/login.php
+++ users/login.php
@@ -7,39 +7,46 @@
     <title>Login</title>
     <link rel="stylesheet" type="text/css" href="style.css">
     <style>
-        body{ font: 14px sans-serif; height: 100vh; display: flex;}
+        body{ font: 14px sans-serif; height: 100vh;}
         .wrapper{ width: 360px; padding: 20px; margin: auto; }
     </style>
 </head>
+<div id="nav">
+    <ul>
+        <li><a href="../">Gameserver</a></li>
+        <div style="width: 100%;"></div>
+</div>
 <body>
-<div class="wrapper">
-    <h2>Login</h2>
+<div style="height: calc(100vh - 58px);display: flex;">
+    <div class="wrapper">
+        <h2>Login</h2>
 
-    <form method="post" action="login.php">
+        <form method="post" action="login.php">
 
-        <?php include('error.php'); ?>
+            <?php include('error.php'); ?>
 
-        <div class="form-group">
-            <label>Username</label>
-            <input type="text" name="username" class="form-control">
-        </div>
-        <div class="form-group">
-            <label>Password</label>
-            <input type="password" name="password" class="form-control">
-        </div>
-        <div class="form-group">
-            <button type="submit" class="btn btn-primary"
-                    name="login_user">
-                Login
-            </button>
-        </div>
-        <p>
-            New Here?
-            <a href="register.php">
-                Click here to register!
-            </a>
-        </p>
-    </form>
+            <div class="form-group">
+                <label>Username</label>
+                <input type="text" name="username" class="form-control">
+            </div>
+            <div class="form-group">
+                <label>Password</label>
+                <input type="password" name="password" class="form-control">
+            </div>
+            <div class="form-group">
+                <button type="submit" class="btn btn-primary"
+                        name="login_user">
+                    Login
+                </button>
+            </div>
+            <p>
+                New Here?
+                <a href="register.php">
+                    Click here to register!
+                </a>
+            </p>
+        </form>
+    </div>
 </div>
 </body>
 
