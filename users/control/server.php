5fa55188aad9cd995c975077f76c39f0b8fef4f9
AunePVP <leo.mhs@icloud.com>
2022-06-09 14:54:40 -0700

    Add plus sign svg

--- users/control/server.php
+++ users/control/server.php
@@ -199,9 +199,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 <div class="serversnippet flex" onclick="location.href='server.php?id=addserver';">
                     <div class="content">
                         <div class="name" style="font-size: 32px;padding-top: 25px;">Add a server</div>
-                        <div style="font-size: 120px;text-align: center;font-weight: 800;font-family: Andale Mono, sans-serif;">
-                            +
-                        </div>
+                        <svg class="svg-circleplus" viewBox="0 0 100 100">
+                            <line x1="32.5" y1="50" x2="67.5" y2="50" stroke-width="5"></line>
+                            <line x1="50" y1="32.5" x2="50" y2="67.5" stroke-width="5"></line>
+                        </svg>
                     </div>
                 </div>
             </div>
