8382bab0223017661c7a54453de8bbfe965e4586
aunepvp <leo.mhs@icloud.com>
2022-06-24 16:47:49 -0700

    Add Timer

--- users/control/script.js
+++ users/control/script.js
@@ -52,3 +52,18 @@ function tab(clicked_id) {
     }
     z.style.textDecoration = "underline";
 }
+function startTime() {
+    const today = new Date();
+    let h = today.getHours();
+    let m = today.getMinutes();
+    let s = today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('clock').innerHTML =  h + ":" + m;
+    setTimeout(startTime, 1000);
+}
+
+function checkTime(i) {
+    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+    return i;
+}
