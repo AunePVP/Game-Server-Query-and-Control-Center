28b83770f822d5b68133146d5024698b61b7a4c1
aunepvp <leo.mhs@icloud.com>
2022-07-01 18:11:17 -0700

    Create login.php

--- users/login.php
+++ users/login.php
@@ -1,53 +1,52 @@
-<?php include('connection.php') ?>
-<!DOCTYPE html>
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+include_once '../html/config.php';
+$title = "Login";
+session_start();
+if (isset($_SESSION['username'])) {
+    header('location: ../');
+}
+if (array_key_exists('login', $_POST)) {
+    $username = trim($_POST["username"]);
+    $password = trim($_POST["password"]);
+    if (!preg_match("/^[a-zA-Z0-9_-äöüß]*$/m", $username)) {
+        $error['specialuser'] = "Special characters are not allowed in user names.";
+    }
+    if (!isset($errors)) {
+        $password = hash('sha256', $password);
+        $conn = mysqli_connect($DB_SERVER, $DB_USERNAME, $DB_PASSWORD, $DB_NAME);
+        $sql = "SELECT * FROM users WHERE username='$username' AND password='$password'";
+        $result = $conn->query($sql);
+        if (mysqli_num_rows($result) == 1) {
+            $_SESSION['username'] = $username;
+            header('location: ../');
+        } else {
+            $error['nomatch'] = "Username or password doesn't match.";
+        }
+    }
+}
+?>
+<!doctype html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
-    <link rel="stylesheet" type="text/css" href="style.css">
-    <style>
-        body{ font: 14px sans-serif; height: 100vh;}
-        .wrapper{ width: 360px; padding: 20px; margin: auto; }
-    </style>
+    <meta charset="UTF-8" >
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title><?php echo $title?></title>
 </head>
-<div id="nav">
-    <ul>
-        <li><a href="../">Gameserver</a></li>
-        <div style="width: 100%;"></div>
-</div>
 <body>
-<div style="height: calc(100vh - 58px);display: flex;">
-    <div class="wrapper">
-        <h2>Login</h2>
-
-        <form method="post" action="login.php">
-
-            <?php include('error.php'); ?>
-
-            <div class="form-group">
-                <label>Username</label>
-                <input type="text" name="username" class="form-control">
-            </div>
-            <div class="form-group">
-                <label>Password</label>
-                <input type="password" name="password" class="form-control">
-            </div>
-            <div class="form-group">
-                <button type="submit" class="btn btn-primary"
-                        name="login_user">
-                    Login
-                </button>
-            </div>
-            <p>
-                New Here?
-                <a href="register.php">
-                    Click here to register!
-                </a>
-            </p>
-        </form>
-    </div>
+<div class="center">
+    <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" style="margin:0">
+        <div class="input"><label for="username">Username:</label><input id="username" class="inputstyle" name="username" type="text" minlength="4" maxlength="15" placeholder="xxxxx" autocomplete="off"></div>
+        <div class="input"><label for="password">Password:</label><input id="password" class="inputstyle" name="password" type="password" minlength="8" placeholder="xxxxxxxxxxxx" autocomplete="off"></div>
+        <div style="display:flex;justify-content: flex-end;-webkit-align-items: center;align-items: center;">
+            <?php if (isset($error['specialuser'])){echo $error['specialuser'];}elseif (isset($error['nomatch'])){echo $error['nomatch'];}?>
+            <input class="button" type="submit" name="login" value="Submit" style="display: <?php echo $displaysubmit?>">
+        </div>
+    </form>
 </div>
 </body>
-
 </html>
